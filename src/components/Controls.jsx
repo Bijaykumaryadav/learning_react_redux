8867b8f6c6e7882dda5f1410c4e7531e5b23008d
bijay kumar yadav <ybijayyadav468@gmail.com>
2024-06-16 08:26:28 +0530

    making counter using redux

--- src/components/Controls.jsx
+++ src/components/Controls.jsx
@@ -1,40 +1,31 @@
 import { useRef } from "react";
-// import { useDispatch } from "react-redux";
-import { counterActions } from "../store";
+import { counterActions } from "../store/counter";
+import { privacyActions } from "../store/privacy";
+import { useDispatch } from "react-redux";
 
 const Controls = () => {
-  // const dispatch = useDispatch();
+  const dispatch = useDispatch();
   const inputElement = useRef();
 
   const handleIncreament = () => {
-    counterActions.increment();
+    dispatch(counterActions.increment());
   };
 
   const handleDecreament = () => {
-    counterActions.decrement;
+    dispatch(counterActions.decrement());
   };
 
   const handlePrivacyToggle = () => {
-    dispatch({ type: "PRIVACY_TOGGLE" });
+    dispatch(privacyActions.toggle())
   };
 
   const handleAdd = () => {
-    dispatch({
-      type: "ADD",
-      payload: {
-        num: inputElement.current.value,
-      },
-    });
+    dispatch(counterActions.add(inputElement.current.value));
     inputElement.current.value = "";
   };
 
   const handleSubstract = () => {
-    dispatch({
-      type: "SUBTRACT",
-      payload: {
-        num: inputElement.current.value,
-      },
-    });
+    dispatch(counterActions.subtract(inputElement.current.value));
     inputElement.current.value = "";
   };
 
